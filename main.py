bae405d78c594374ac4d200f3fb55882f6e81270
nboj <60493251+nboj@users.noreply.github.com>
2025-07-04 11:31:36 -0400

    u

--- main.py
+++ main.py
@@ -138,13 +138,19 @@ if __name__ == "__main__":
             torch.cuda.empty_cache()
             activations, layers, handles = [], [], []
 
+        t = scheduler.timesteps[-1]
         for name, layer in unet.named_modules():
             handles.append(layer.register_forward_hook(save_activation(name,layer)))
 
+        # expand the latents if we are doing classifier-free guidance to avoid doing two forward passes.
         latent_model_input = torch.cat([latents] * 2)
         latent_model_input = scheduler.scale_model_input(latent_model_input, timestep=t)
 
-        noise_pred, samps, time, weights = sd.forward_unet(unet, latent_model_input, t, text_embeddings)
+        initial_latents = latent_model_input
+
+        with torch.no_grad():
+          # predict the noise residual
+          noise_pred, samples, time, weights = sd.forward_unet(unet, latent_model_input, t, text_embeddings)
 
         # perform guidance
         noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
@@ -152,39 +158,60 @@ if __name__ == "__main__":
 
         # compute the previous noisy sample x_t -> x_t-1
         latents = scheduler.step(noise_pred, t, latents).prev_sample
+        # scale and decode the image latents with vae
 
         for handle in handles:
-            handle.remove()
+          handle.remove()
 
-        if lays is None:
-            lays = layers
+        X = 1 / 0.18215 * latents
+        with torch.no_grad():
+            image = vae.decode(X).sample
+        image = (image / 2 + 0.5).clamp(0, 1).squeeze()
+        image = (image.permute(1, 2, 0) * 255).to(torch.uint8).cpu().numpy()
+        image = Image.fromarray(image)
+        image
 
-    # scale and decode the image latents with vae
+
+    R, queries, keys, values, weights = utils.apply_lrp(unet, vae, layers, activations, samples, time, text_embeddings, initial_latents, weights)
+
+
+    uncond, text = (utils.norm_rel(R)*1e5).chunk(2)
+    comb = 1 / 0.18215 *(uncond + guidance_scale * (text - uncond))
+    im = vae.decode(comb).sample
+    plt.imshow(im[0].detach().cpu().sum(axis=0)
+
+
+    uncond, text = (utils.norm_rel(R)*1e5).chunk(2)
+    comb = 1 / 0.18215 *(uncond + guidance_scale * (text - uncond))
+    text = 1 / 0.18215 *text
+    uncond = 1 / 0.18215 *uncond
     X = 1 / 0.18215 * latents
+    utils.debug_relevance_scores(uncond)
+    utils.debug_relevance_scores(text)
+    utils.debug_relevance_scores(R)
+
+    # noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
     with torch.no_grad():
-        image = vae.decode(X).sample
+      lrp1 = vae.decoder(vae.post_quant_conv(text))
+      lrp2 = vae.decoder(vae.post_quant_conv(uncond))
+      lrp3 = vae.decoder(vae.post_quant_conv(comb))
+      image = vae.decoder(vae.post_quant_conv(X))
+
     image = (image / 2 + 0.5).clamp(0, 1).squeeze()
-    image = (image.permute(1, 2, 0) * 255).to(torch.uint8).cpu().numpy()
-    image = Image.fromarray(image)
-    image
+    image = (image.permute(1, 2, 0) * 255).to(torch.uint8).cpu()
 
+    utils.debug_relevance_scores(lrp3)
 
+    utils.heatmap(lrp1[0].cpu().sum(axis=0), 5, 5)
+    utils.heatmap(lrp2[0].cpu().sum(axis=0), 5, 5)
+    utils.heatmap(lrp3[0].cpu().sum(axis=0), 5, 5)
+    utils.pos_heatmap(lrp3[0].cpu().sum(axis=0), image,5)
+    utils.neg_heatmap(lrp3[0].cpu().sum(axis=0),image,5)
+    utils.comb_heatmap(lrp3[0].cpu().sum(axis=0),5)
+    utils.heatmap(lrp3[0].cpu().sum(axis=0), 5, 5, True)
 
-
-
-    for handle in handles:
-        handle.remove()
-
-    prev, q, k, v, w = utils.apply_lrp(unet, vae, lays, activations, samps, time, text_embeddings, latent_model_input, weights)
-    prev = utils.norm_rel(prev)
-    R.append(prev.detach().cpu())
-    values.append(k)
-    keys.append(k)
-    del activations, samples, time, initial_latents, weights, q, k, v
-
-
-
-
+    utils.heatmap(comb[0].detach().cpu().sum(axis=0), 5, 5)
+    utils.heatmap(comb[0].detach().cpu().sum(axis=0), 5, 5, True
 
 
 
